ac5db2018292f8f3ea3b76a405de67f8f447b70f
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-21 18:00:39 +0900

    コントローラへの追記(ボタン判別処理)

--- src/main/java/com/example/demo/controller/PathValiableController.java
+++ src/main/java/com/example/demo/controller/PathValiableController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PathValiableController {
@@ -32,5 +33,26 @@ public class PathValiableController {
 		}
 		// 戻り値は「ビュー名」を返す
 		return view;
+	}
+	
+	/** 「ボタンA」押下処理 */
+	@PostMapping(value = "send", params = "a")
+	public String showAView() {
+		// 戻り値は「ビュー名」を返す
+		return "submit/a";
+	}
+	
+	/** 「ボタンB」押下処理 */
+	@PostMapping(value = "send", params = "b")
+	public String showBView() {
+		// 戻り値は「ビュー名」を返す
+		return "submit/b";
+	}
+	
+	/** 「ボタンC」押下処理 */
+	@PostMapping(value = "send", params = "c")
+	public String showCView() {
+		// 戻り値は「ビュー名」を返す
+		return "submit/c";
 	}
 }
